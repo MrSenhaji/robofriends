dee3e252f532948f76271c76645405acfac4e1d2
MrSenhaji <senhajimohamed.20@gmail.com>
2020-08-16 13:49:36 +0100

    adding files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import CardList from './CardList';
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <CardList robots={robots} />
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/robots.js
@@ -0,0 +1,501 @@
+export const robots = [{
+    "id": 1,
+    "name": "Pillinger",
+    "username": "tpillinger0",
+    "email": "epillinger0@cdbaby.com"
+}, {
+    "id": 2,
+    "name": "Keable",
+    "username": "ckeable1",
+    "email": "ekeable1@cnbc.com"
+}, {
+    "id": 3,
+    "name": "Menezes",
+    "username": "gmenezes2",
+    "email": "cmenezes2@liveinternet.ru"
+}, {
+    "id": 4,
+    "name": "Bettles",
+    "username": "abettles3",
+    "email": "mbettles3@furl.net"
+}, {
+    "id": 5,
+    "name": "Malshinger",
+    "username": "tmalshinger4",
+    "email": "amalshinger4@diigo.com"
+}, {
+    "id": 6,
+    "name": "Locarno",
+    "username": "dlocarno5",
+    "email": "glocarno5@cdbaby.com"
+}, {
+    "id": 7,
+    "name": "Lewsie",
+    "username": "dlewsie6",
+    "email": "tlewsie6@youku.com"
+}, {
+    "id": 8,
+    "name": "Feldberger",
+    "username": "dfeldberger7",
+    "email": "kfeldberger7@icq.com"
+}, {
+    "id": 9,
+    "name": "Serrier",
+    "username": "fserrier8",
+    "email": "tserrier8@digg.com"
+}, {
+    "id": 10,
+    "name": "Andrei",
+    "username": "aandrei9",
+    "email": "tandrei9@hp.com"
+}, {
+    "id": 11,
+    "name": "Van Haeften",
+    "username": "jvanhaeftena",
+    "email": "tvanhaeftena@weibo.com"
+}, {
+    "id": 12,
+    "name": "Zanicchi",
+    "username": "dzanicchib",
+    "email": "hzanicchib@lycos.com"
+}, {
+    "id": 13,
+    "name": "Stirley",
+    "username": "astirleyc",
+    "email": "cstirleyc@shareasale.com"
+}, {
+    "id": 14,
+    "name": "McVitie",
+    "username": "cmcvitied",
+    "email": "amcvitied@patch.com"
+}, {
+    "id": 15,
+    "name": "MacFarlan",
+    "username": "smacfarlane",
+    "email": "gmacfarlane@imageshack.us"
+}, {
+    "id": 16,
+    "name": "Wadley",
+    "username": "ewadleyf",
+    "email": "wwadleyf@aol.com"
+}, {
+    "id": 17,
+    "name": "Penddreth",
+    "username": "spenddrethg",
+    "email": "mpenddrethg@chronoengine.com"
+}, {
+    "id": 18,
+    "name": "Cottage",
+    "username": "fcottageh",
+    "email": "mcottageh@topsy.com"
+}, {
+    "id": 19,
+    "name": "Birbeck",
+    "username": "cbirbecki",
+    "email": "cbirbecki@unicef.org"
+}, {
+    "id": 20,
+    "name": "Davidman",
+    "username": "edavidmanj",
+    "email": "wdavidmanj@ebay.co.uk"
+}, {
+    "id": 21,
+    "name": "Pasquale",
+    "username": "hpasqualek",
+    "email": "lpasqualek@parallels.com"
+}, {
+    "id": 22,
+    "name": "Jiri",
+    "username": "tjiril",
+    "email": "ajiril@reverbnation.com"
+}, {
+    "id": 23,
+    "name": "Louys",
+    "username": "klouysm",
+    "email": "olouysm@furl.net"
+}, {
+    "id": 24,
+    "name": "Thorogood",
+    "username": "ithorogoodn",
+    "email": "athorogoodn@indiegogo.com"
+}, {
+    "id": 25,
+    "name": "Bellany",
+    "username": "sbellanyo",
+    "email": "gbellanyo@usnews.com"
+}, {
+    "id": 26,
+    "name": "Yarmouth",
+    "username": "fyarmouthp",
+    "email": "jyarmouthp@state.tx.us"
+}, {
+    "id": 27,
+    "name": "Bolver",
+    "username": "kbolverq",
+    "email": "sbolverq@blog.com"
+}, {
+    "id": 28,
+    "name": "Pryer",
+    "username": "cpryerr",
+    "email": "bpryerr@live.com"
+}, {
+    "id": 29,
+    "name": "Tilzey",
+    "username": "ptilzeys",
+    "email": "btilzeys@yandex.ru"
+}, {
+    "id": 30,
+    "name": "Tarquini",
+    "username": "ptarquinit",
+    "email": "mtarquinit@mysql.com"
+}, {
+    "id": 31,
+    "name": "Btham",
+    "username": "kbthamu",
+    "email": "sbthamu@clickbank.net"
+}, {
+    "id": 32,
+    "name": "Scotney",
+    "username": "fscotneyv",
+    "email": "lscotneyv@mozilla.com"
+}, {
+    "id": 33,
+    "name": "Vitet",
+    "username": "cvitetw",
+    "email": "rvitetw@miitbeian.gov.cn"
+}, {
+    "id": 34,
+    "name": "Plumb",
+    "username": "mplumbx",
+    "email": "gplumbx@guardian.co.uk"
+}, {
+    "id": 35,
+    "name": "Haggata",
+    "username": "dhaggatay",
+    "email": "mhaggatay@wiley.com"
+}, {
+    "id": 36,
+    "name": "Lansdale",
+    "username": "alansdalez",
+    "email": "mlansdalez@adobe.com"
+}, {
+    "id": 37,
+    "name": "Berthod",
+    "username": "lberthod10",
+    "email": "bberthod10@techcrunch.com"
+}, {
+    "id": 38,
+    "name": "Haug",
+    "username": "shaug11",
+    "email": "bhaug11@hud.gov"
+}, {
+    "id": 39,
+    "name": "Clayworth",
+    "username": "hclayworth12",
+    "email": "dclayworth12@sogou.com"
+}, {
+    "id": 40,
+    "name": "Lander",
+    "username": "blander13",
+    "email": "clander13@webmd.com"
+}, {
+    "id": 41,
+    "name": "Easman",
+    "username": "measman14",
+    "email": "measman14@microsoft.com"
+}, {
+    "id": 42,
+    "name": "Loxton",
+    "username": "dloxton15",
+    "email": "tloxton15@amazon.com"
+}, {
+    "id": 43,
+    "name": "Campione",
+    "username": "ecampione16",
+    "email": "gcampione16@symantec.com"
+}, {
+    "id": 44,
+    "name": "Hackett",
+    "username": "bhackett17",
+    "email": "mhackett17@feedburner.com"
+}, {
+    "id": 45,
+    "name": "Carff",
+    "username": "icarff18",
+    "email": "gcarff18@friendfeed.com"
+}, {
+    "id": 46,
+    "name": "Lente",
+    "username": "blente19",
+    "email": "blente19@pinterest.com"
+}, {
+    "id": 47,
+    "name": "Geeves",
+    "username": "bgeeves1a",
+    "email": "ngeeves1a@reverbnation.com"
+}, {
+    "id": 48,
+    "name": "Delagua",
+    "username": "sdelagua1b",
+    "email": "rdelagua1b@marriott.com"
+}, {
+    "id": 49,
+    "name": "Turnock",
+    "username": "jturnock1c",
+    "email": "lturnock1c@boston.com"
+}, {
+    "id": 50,
+    "name": "MacIlurick",
+    "username": "tmacilurick1d",
+    "email": "tmacilurick1d@multiply.com"
+}, {
+    "id": 51,
+    "name": "Welford",
+    "username": "iwelford1e",
+    "email": "gwelford1e@umich.edu"
+}, {
+    "id": 52,
+    "name": "Burwell",
+    "username": "rburwell1f",
+    "email": "eburwell1f@spotify.com"
+}, {
+    "id": 53,
+    "name": "Gallifont",
+    "username": "egallifont1g",
+    "email": "wgallifont1g@mayoclinic.com"
+}, {
+    "id": 54,
+    "name": "Davydoch",
+    "username": "ddavydoch1h",
+    "email": "cdavydoch1h@salon.com"
+}, {
+    "id": 55,
+    "name": "Sparhawk",
+    "username": "tsparhawk1i",
+    "email": "ssparhawk1i@prnewswire.com"
+}, {
+    "id": 56,
+    "name": "Threadgould",
+    "username": "kthreadgould1j",
+    "email": "lthreadgould1j@hostgator.com"
+}, {
+    "id": 57,
+    "name": "Kunkler",
+    "username": "fkunkler1k",
+    "email": "dkunkler1k@blogger.com"
+}, {
+    "id": 58,
+    "name": "Lethby",
+    "username": "mlethby1l",
+    "email": "clethby1l@slideshare.net"
+}, {
+    "id": 59,
+    "name": "Hinks",
+    "username": "rhinks1m",
+    "email": "nhinks1m@illinois.edu"
+}, {
+    "id": 60,
+    "name": "Burdis",
+    "username": "bburdis1n",
+    "email": "vburdis1n@hexun.com"
+}, {
+    "id": 61,
+    "name": "Cullon",
+    "username": "acullon1o",
+    "email": "gcullon1o@163.com"
+}, {
+    "id": 62,
+    "name": "McMurthy",
+    "username": "hmcmurthy1p",
+    "email": "amcmurthy1p@dot.gov"
+}, {
+    "id": 63,
+    "name": "Spellissy",
+    "username": "mspellissy1q",
+    "email": "aspellissy1q@tmall.com"
+}, {
+    "id": 64,
+    "name": "Caughan",
+    "username": "bcaughan1r",
+    "email": "ocaughan1r@de.vu"
+}, {
+    "id": 65,
+    "name": "Gorringe",
+    "username": "bgorringe1s",
+    "email": "mgorringe1s@gravatar.com"
+}, {
+    "id": 66,
+    "name": "Batterson",
+    "username": "lbatterson1t",
+    "email": "lbatterson1t@elpais.com"
+}, {
+    "id": 67,
+    "name": "Loughrey",
+    "username": "jloughrey1u",
+    "email": "yloughrey1u@globo.com"
+}, {
+    "id": 68,
+    "name": "Hostan",
+    "username": "bhostan1v",
+    "email": "dhostan1v@reverbnation.com"
+}, {
+    "id": 69,
+    "name": "Hubert",
+    "username": "lhubert1w",
+    "email": "ghubert1w@salon.com"
+}, {
+    "id": 70,
+    "name": "Galton",
+    "username": "dgalton1x",
+    "email": "mgalton1x@nps.gov"
+}, {
+    "id": 71,
+    "name": "Phythean",
+    "username": "sphythean1y",
+    "email": "gphythean1y@macromedia.com"
+}, {
+    "id": 72,
+    "name": "Gascard",
+    "username": "bgascard1z",
+    "email": "lgascard1z@opera.com"
+}, {
+    "id": 73,
+    "name": "Ibbison",
+    "username": "cibbison20",
+    "email": "fibbison20@gnu.org"
+}, {
+    "id": 74,
+    "name": "Morrel",
+    "username": "smorrel21",
+    "email": "bmorrel21@printfriendly.com"
+}, {
+    "id": 75,
+    "name": "Shelp",
+    "username": "ashelp22",
+    "email": "kshelp22@odnoklassniki.ru"
+}, {
+    "id": 76,
+    "name": "Poleykett",
+    "username": "zpoleykett23",
+    "email": "dpoleykett23@4shared.com"
+}, {
+    "id": 77,
+    "name": "Wiltshire",
+    "username": "rwiltshire24",
+    "email": "nwiltshire24@trellian.com"
+}, {
+    "id": 78,
+    "name": "Szabo",
+    "username": "mszabo25",
+    "email": "lszabo25@ask.com"
+}, {
+    "id": 79,
+    "name": "Vogelein",
+    "username": "tvogelein26",
+    "email": "lvogelein26@mlb.com"
+}, {
+    "id": 80,
+    "name": "Behne",
+    "username": "tbehne27",
+    "email": "cbehne27@zdnet.com"
+}, {
+    "id": 81,
+    "name": "Pecey",
+    "username": "kpecey28",
+    "email": "cpecey28@wordpress.com"
+}, {
+    "id": 82,
+    "name": "Rhule",
+    "username": "rrhule29",
+    "email": "brhule29@youtube.com"
+}, {
+    "id": 83,
+    "name": "Stobbe",
+    "username": "tstobbe2a",
+    "email": "sstobbe2a@ucoz.ru"
+}, {
+    "id": 84,
+    "name": "Tingly",
+    "username": "rtingly2b",
+    "email": "ktingly2b@earthlink.net"
+}, {
+    "id": 85,
+    "name": "Edmund",
+    "username": "hedmund2c",
+    "email": "eedmund2c@google.de"
+}, {
+    "id": 86,
+    "name": "Hearse",
+    "username": "rhearse2d",
+    "email": "chearse2d@wsj.com"
+}, {
+    "id": 87,
+    "name": "Oriel",
+    "username": "eoriel2e",
+    "email": "foriel2e@abc.net.au"
+}, {
+    "id": 88,
+    "name": "Charlewood",
+    "username": "tcharlewood2f",
+    "email": "jcharlewood2f@census.gov"
+}, {
+    "id": 89,
+    "name": "Pauls",
+    "username": "jpauls2g",
+    "email": "kpauls2g@umn.edu"
+}, {
+    "id": 90,
+    "name": "Crichley",
+    "username": "acrichley2h",
+    "email": "kcrichley2h@salon.com"
+}, {
+    "id": 91,
+    "name": "Roaf",
+    "username": "aroaf2i",
+    "email": "wroaf2i@networksolutions.com"
+}, {
+    "id": 92,
+    "name": "Buxey",
+    "username": "ebuxey2j",
+    "email": "rbuxey2j@friendfeed.com"
+}, {
+    "id": 93,
+    "name": "Petrowsky",
+    "username": "spetrowsky2k",
+    "email": "rpetrowsky2k@yahoo.com"
+}, {
+    "id": 94,
+    "name": "Detloff",
+    "username": "jdetloff2l",
+    "email": "adetloff2l@discuz.net"
+}, {
+    "id": 95,
+    "name": "Ilbert",
+    "username": "oilbert2m",
+    "email": "bilbert2m@angelfire.com"
+}, {
+    "id": 96,
+    "name": "Bowkett",
+    "username": "ebowkett2n",
+    "email": "cbowkett2n@prlog.org"
+}, {
+    "id": 97,
+    "name": "Rosa",
+    "username": "srosa2o",
+    "email": "mrosa2o@linkedin.com"
+}, {
+    "id": 98,
+    "name": "Trimmill",
+    "username": "otrimmill2p",
+    "email": "btrimmill2p@rakuten.co.jp"
+}, {
+    "id": 99,
+    "name": "Renvoise",
+    "username": "prenvoise2q",
+    "email": "krenvoise2q@constantcontact.com"
+}, {
+    "id": 100,
+    "name": "Beel",
+    "username": "cbeel2r",
+    "email": "ebeel2r@dot.gov"
+}];
